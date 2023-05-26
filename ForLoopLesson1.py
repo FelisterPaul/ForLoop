2860fba1565f6b32338dd46771c7e4146787d46a
stecy1 <stecy@solutech.co.ke>
2023-05-26 07:41:12 +0300

    commit

---
+++ ForLoopLesson1.py
@@ -0,0 +1,10 @@
+numbers = [1, 2, 3, 4, 5]
+for item in numbers:
+    print(item)
+
+#We can also use while loop to iterate a list in it's own line.
+
+i = 0
+while i < len(numbers):
+    print(numbers[i])
+    i = i + 1
